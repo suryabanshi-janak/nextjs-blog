fa54c0bee105e926296a45678ec648ca67a2751e
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-08-15 23:05:06 +0545

    Correcting the image path

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -5,7 +5,7 @@ const ProfileImage = () => {
     <section className='w-full mx-auto'>
       <Image
         className='border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
-        src='/profile.jpg'
+        src='/images/profile.jpg'
         width={200}
         height={200}
         alt='Dave Gray'
